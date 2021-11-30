954766ce566bbcd892d81c3cf8295b25d9b8f72b
rob0302 <rob0302@knighrs.ucf.edu>
2021-11-29 20:16:14 -0500

    done

---
+++ EX8.cpp
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 ROBERT MCQUEEN
+ */
+#include "std_lib_facilities.h"
+
+int main(){
+int number =3;
+
+if(number % 2 == 0){
+cout<<"The number "<<number<<" is even"<<endl;
+}
+else if(number % 2 != 0){
+
+cout<<"The number "<<number<<" is odd"<<endl;
+}
+
+}
